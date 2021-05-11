00210b9f3344cf9772c2125256414f26a041384b
admin admin <evaladmin@example.com>
2021-05-11 22:04:06 +0000

    User admin wrote content /scripts/rest/conditions.get.groovy

--- scripts/rest/conditions.get.groovy
+++ scripts/rest/conditions.get.groovy
@@ -1,3 +1,42 @@
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.FieldSortBuilder
+import org.elasticsearch.search.sort.SortOrder
+
 def result = [:]
+
+def queryStatement = 'content-type:"/component/product"'
+
+def builder = new SearchSourceBuilder().query(QueryBuilders.queryStringQuery(queryStatement))
+
+// execute query
+def executedQuery = elasticsearch.search(new SearchRequest().source(builder))
+
 result.conditions = []
+
+def elasticResults = executedQuery.hits.hits*.getSourceAsMap()
+elasticResults.eachWithIndex { document, idx ->
+	def condition = [ id:             idx,
+    				cmsId:          document.localId,
+                    title:          document.title_s, 
+                    description:    document.description_t,
+                  ]
+    
+	result.conditions.add(condition)
+}
+
 return result
+
+
+
+def getDesc(document) {
+	// simple example of abstracting where price comes from
+	return new Float(document.description_t)
+}
+
+def getInventory(document) {
+	// simple example of abstracting where inventory comes from
+	return new Integer(document.installments)
+}
+    
